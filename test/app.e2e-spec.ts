2e0c8317773c46c53229d3484a97d1e39b3a8346
Hamzah Ahmad <ahmadhamzah500@gmail.com>
2024-07-20 18:01:42 -0500

    cleanup: removes unused test file

--- test/app.e2e-spec.ts
+++
@@ -1,8 +0,0 @@
-import * as request from 'supertest';
-import { server } from './setup';
-
-describe('AppController (e2e)', () => {
-  it('/ (GET)', () => {
-    return request(server).get('/').expect(200).expect('This is a public route');
-  });
-});
