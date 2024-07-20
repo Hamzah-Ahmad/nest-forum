30e3364f39e9625ab70ac2d84dc8af4d7d6dad6e
Hamzah Ahmad <ahmadhamzah500@gmail.com>
2024-07-20 18:00:55 -0500

    cleanup: removes unused routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,34 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-import { Public } from './auth/decorators/public.decorator';
-import { Role, User } from './user/entities/User.entity';
-import { CurrentUser } from './auth/decorators/current-user.decorator';
-import { Roles } from './auth/decorators/roles.decorator';
+import { Controller } from '@nestjs/common';
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  @Public()
-  getPublic(): string {
-    return this.appService.getPublic();
-  }
-  @Get('/private')
-  getPrivate(): string {
-    return this.appService.getPrivate();
-  }
-  @Get('/profile')
-  getProfile(@CurrentUser() user: User): User {
-    return this.appService.getProfile(user);
-  }
-  @Get('/adminOnly')
-  @Roles(Role.Admin)
-  getAdminOnly(@CurrentUser() user: User): string {
-    return this.appService.getAdminOnly();
-  }
-  @Get('/modRoute')
-  @Roles(Role.Moderator)
-  getModRoute(@CurrentUser() user: User): string {
-    return this.appService.getModRoute();
-  }
+  constructor() {}
 }
